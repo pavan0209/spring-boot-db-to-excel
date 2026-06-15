e599712441ba2eac2d240bebb25493f8731f7e99
pavan0209 <pavanasonawane@gmail.com>
2026-06-15 22:27:28 +0530

    add download-data api to download generated excel file

---
+++ src/main/java/com/pavan/coding/spring_boot_db_to_excel/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.pavan.coding.spring_boot_db_to_excel.controller;
+
+import com.pavan.coding.spring_boot_db_to_excel.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
+    @GetMapping("/download-data")
+    public ResponseEntity<Resource> download() throws IOException {
+
+        String fileName = "employees.xlsx";
+        ByteArrayInputStream data = employeeService.getData();
+        InputStreamResource file = new InputStreamResource(data);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName)
+                .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                .body(file);
+    }
+}
